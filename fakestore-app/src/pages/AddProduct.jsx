d0455e6c659a3103400b24d6ce4b4f4f6ffc5d24
Gianna Cosenza <giacosey28@gmail.com>
2026-06-17 15:41:41 -0400

    Added imports. Defined AddProduct function. defined form data categories. Defined handlechange and handlesubmit

--- fakestore-app/src/pages/AddProduct.jsx
+++ fakestore-app/src/pages/AddProduct.jsx
@@ -1,4 +1,26 @@
-import { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { Container, Form, Button, Alert, Spinner } from 'react-bootstrap';
+import { useState } from 'react';
+import { Container, Form, Button, Alert } from 'react-bootstrap';
 import axios from 'axios';
+
+function AddProduct() {
+    const [formData, setFormData] = useState({
+        title: '',
+        price: '',
+        description: '',
+        category: ''
+    });
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError(null);
+        axios.post('https://fakestoreapi.com/products', formData)
+            .then(() => setSuccess(true))
+            .catch(() => setError('Failed to add product. Please try again.'))
+    }
+}
