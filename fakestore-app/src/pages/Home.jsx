83f8d654ef599b58ce9ddce90a441663133bba8f
Gianna Cosenza <giacosey28@gmail.com>
2026-06-17 13:48:07 -0400

    Added home function using bootstrap styling and made the heading, intro text, and navigation button.

---
+++ fakestore-app/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Container, Form, Button, Alert, Spinner } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+function Home() {
+    return (
+        <Container className="text-centermt-5">
+            <h1>Welcome to the Fake Store</h1>
+            <p>Explore our wide range of products and enjoy shopping!</p>
+            <Button as={Link} to="/products" variant="primary">Shop Now</Button>
+        </Container>
+    );
+}
+
+export default Home;
