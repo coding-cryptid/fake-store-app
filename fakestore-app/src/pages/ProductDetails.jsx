dd7d01405e66330697c8ad021a24fa2c98e637be
Gianna Cosenza <giacosey28@gmail.com>
2026-06-17 15:18:29 -0400

    added imports and created Product Details function. defined states.

--- fakestore-app/src/pages/ProductDetails.jsx
+++ fakestore-app/src/pages/ProductDetails.jsx
@@ -1,5 +1,12 @@
-import { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { Container, Row, Col, Button, Spinner, Alert, Modal } from 'react-bootstrap';
+import { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Container, Row, Col, Button, Spinner, Alert } from 'react-bootstrap';
 import axios from 'axios';
 
+function ProductDetails() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+}
