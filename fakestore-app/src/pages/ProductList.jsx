711d4bc4d397fd4336709b0ebcb58abcb4490e60
Gianna Cosenza <giacosey28@gmail.com>
2026-06-17 14:01:52 -0400

    Defined the the three states for products: an empty array, for loading: its set as true, and for an error it starts null.

--- fakestore-app/src/pages/ProductList.jsx
+++ fakestore-app/src/pages/ProductList.jsx
@@ -3,3 +3,10 @@ import { Container, Row, Spinner, Alert } from 'react-bootstrap';
 import axios from 'axios';
 import ProductCard from '../components/ProductCard';
 
+function ProductList() {
+    const [products, setProducts] = UseState([]);
+    const [loading, SetLoading] = useState(true);
+    const [error, SetError] = UseState(null);
+}
+
+export default ProductList;
