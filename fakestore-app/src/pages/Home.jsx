09bd77d957daccc53f078de66461a1a4ec3687a8
Gianna Cosenza <giacosey28@gmail.com>
2026-06-19 09:43:54 -0400

    Fixed typo in bootstrap styling

--- fakestore-app/src/pages/Home.jsx
+++ fakestore-app/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <Container className="text-centermt-5">
+        <Container className="text-center mt-5">
             <h1>Welcome to the Fake Store</h1>
             <p>Explore our wide range of products and enjoy shopping!</p>
             <Button as={Link} to="/products" variant="primary">Shop Now</Button>
